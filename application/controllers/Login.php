4c60cc1afb02b8944aadae07dfe0c75514615a55
Gleison Rocha <glucasmr@gmail.com>
2023-03-29 00:15:41 -0300

    feat: validação de login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -27,22 +28,25 @@ class Login extends CI_Controller {
 	public function store()
 	{
 		$this->load->model('login_model');
-
-		$email = $_POST['email'];		
+		$email = $_POST['email'];
 		$password = md5($_POST['password']);
 		$user = $this->login_model->store($email, $password);
-	
-		if($user){
+		if ($user) {
 			$this->session->set_userdata('logged_user', $user);
-			redirect('dashboard');
-		}else{
+			if ($user['is_admin']) {
+				redirect('dashboard');
+			}
+			redirect('tickets');
+		} else {
+			$msg = 'E-mail ou senha incorreto';
+			$this->session->set_flashdata('msg', $msg);
 			redirect('login');
 		}
 	}
 
-	public function logout(){
+	public function logout()
+	{
 		$this->session->unset_userdata('logged_user');
 		redirect('login');
-			
-	}	
+	}
 }
